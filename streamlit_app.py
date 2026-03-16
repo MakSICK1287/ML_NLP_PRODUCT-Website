4358702d19ed48ac4b3b67a8096f1a4226c6dee4
MakSICK1287 <pitalenkomaksim@gmail.com>
2026-03-16 12:59:15 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,45 +7,7 @@ from transformers import AutoTokenizer, AutoModelForTokenClassification, pipelin
 # -----------------------
 # Load the model
 # -----------------------
-hf_repo_id = "MaksICK1287/Product_NLP"
-model_subfolder = "product_ner_model_distilbert_4"
-
-@st.cache_resource(show_spinner=True)
-def load_model():
-    try:
-        with st.spinner("Tokenizer load..."):
-            tokenizer = AutoTokenizer.from_pretrained(
-                hf_repo_id,
-                subfolder=model_subfolder,
-                use_fast=True
-            )
-        
-        with st.spinner("Model load..."):
-            model = AutoModelForTokenClassification.from_pretrained(
-                hf_repo_id,
-                subfolder=model_subfolder  
-            )
-
-        ner_pipe = pipeline(
-            "ner",
-            model=model,
-            tokenizer=tokenizer,
-            aggregation_strategy="simple"
-        )
-        return ner_pipe
-        
-    except Exception as e:
-        st.error(f"Erorr model: {str(e)}")
-        with st.expander("Details"):
-            st.code(str(e))
-        return None
-
-
-ner_pipeline = load_model()
-
-if ner_pipeline is None:
-    st.error("Not able to load the module")
-    st.stop()
+model_name_or_path = "product_ner_model_distilbert_2"
 
 
 @st.cache_resource(show_spinner=True)
